69a983ebb4c4cefb432cd2cd343de1586a3ff727
Izan <izan.hidalgo2006@gmail.com>
2025-12-08 13:36:28 +0100

    Login, Registro, Perfil, Agregar o quitar Juegos, Menu Juegos con filtrado

--- app/src/main/java/com/example/aplicacion_android/GamesSelectorActivity.kt
+++ app/src/main/java/com/example/aplicacion_android/GamesSelectorActivity.kt
@@ -1,27 +1,16 @@
 package com.example.aplicacion_android
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginActivity : AppCompatActivity() {
+class GamesSelectorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-
-        val btnRegister = findViewById<Button>(R.id.btnRegister)
-
-        btnRegister.setOnClickListener {
-            val intent = Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
-        }
-
-
+        setContentView(R.layout.activity_games_selector)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/aplicacion_android/MainActivity.kt
+++ app/src/main/java/com/example/aplicacion_android/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.aplicacion_android
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
