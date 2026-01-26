6d7aff67d005a3e8e6e8b677f73d72c33ceabdfa
Set catalan <2024_set.catalan@iticbcn.cat>
2026-01-26 19:03:07 +0100

    viewmodel acabat

--- app/src/main/java/com/example/aplicacion_android/LoginActivity.kt
+++ app/src/main/java/com/example/aplicacion_android/LoginActivity.kt
@@ -2,29 +2,61 @@ package com.example.aplicacion_android
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel: ViewModel() {
+    private val _nomUsuari = MutableLiveData<String>()
+    private val _contrasenya = MutableLiveData<String>()
+    val nomUsuari: LiveData<String> = _nomUsuari
+    val contrasenya: LiveData<String> = _contrasenya
+
+    fun canviNom(nom: String){
+        _nomUsuari.value = nom
+    }
+
+    fun canviContra(contra: String){
+        _contrasenya.value = contra
+    }
+}
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var btn_close_edit: ImageView
+    private lateinit var btnCloseEdit: ImageView
     private lateinit var btnRegister: Button
     private lateinit var btnConfirmarLogin: Button
+    private lateinit var etUser: EditText
+    private lateinit var etPass: EditText
+
+    private val loginViewModel: LoginViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        loginViewModel.nomUsuari.observe(this){ nomUsuari ->
+            etUser.hint = nomUsuari
+        }
+
+        loginViewModel.contrasenya.observe(this){ contrasenya ->
+            etPass.hint = contrasenya
         }
 
         initComponents()
@@ -32,20 +64,21 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun initComponents() {
-        btn_close_edit = findViewById<ImageView>(R.id.btn_close_edit)
-        btnRegister = findViewById<Button>(R.id.btnRegister)
-        btnConfirmarLogin = findViewById<Button>(R.id.btnConfirmarLogin)
+        btnCloseEdit = findViewById(R.id.btn_close_edit)
+        btnRegister = findViewById(R.id.btnRegister)
+        btnConfirmarLogin = findViewById(R.id.btnConfirmarLogin)
+        etUser = findViewById(R.id.etUser)
+        etPass = findViewById(R.id.etPass)
     }
 
     private fun initListeners() {
-        btn_close_edit.setOnClickListener {
+        btnCloseEdit.setOnClickListener {
             finish()
         }
 
         btnRegister.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
-            finish()
         }
 
         btnConfirmarLogin.setOnClickListener {
@@ -53,5 +86,21 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+        etUser.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(s: Editable?) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                loginViewModel.canviNom(etUser.text.toString())
+            }
+        })
+
+        etPass.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(s: Editable?) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                loginViewModel.canviContra(etPass.text.toString())
+            }
+        })
     }
 }
