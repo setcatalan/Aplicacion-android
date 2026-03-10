4eb44b2338bd1ae555c3cb8834184e1da9176283
1zan06 <izan.hidalgo2006@gmail.com>
2026-03-10 19:44:25 +0100

    Register Retrofit hecho

--- app/src/main/java/com/example/aplicacion_android/User/UsuariService.kt
+++ app/src/main/java/com/example/aplicacion_android/User/UsuariService.kt
@@ -1,10 +1,18 @@
 package com.example.aplicacion_android.User
 
+import com.example.aplicacion_android.Login.UsuariRequest
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface UsuariService {
 
     @GET("get/all")
     suspend fun llistaUsuaris(): Response<List<Usuari>>
+
+    @POST("usuaris/")
+    suspend fun registrarUsuari (
+        @Body nouUsuari: UsuariRequest
+    ): Response<Usuari>
 }
